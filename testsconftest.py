a78534d5d4ccc2d6909e53aaede379c2a65ca9b0
Chidera Progress Nwaokwa <bi78xz@student.sunderland.ac.uk>
2026-04-12 01:38:15 +0100

    Add conftest.py to fix module imports in Jenkins test runner

--- testsconftest.py
+++ testsconftest.py
@@ -1,5 +1,2 @@
 import sys
 import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src', 'automation'))
